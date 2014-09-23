ee2de1fbc8c339dc2fdb600d4a7fe566bac6bef5
Logan <Testing@roxannergossmbp.netgear.com>
2014-09-23 02:04:22 -0700

    Graph is working, will add the other three tomorrow.

--- js/fake_logic.js
+++ js/fake_logic.js
@@ -1,29 +1,59 @@
-var data = [[1, 300], [2, 600], [3, 550], [4, 400], [5, 300]];
-var options = {
-	grid: {
-		borderWidth: 1,
-		minBorderMargin: 20,
-		labelMargin: 10,
-		backgroundColor: {
-			colors: ["#fff", "#e4f4f4"]
-		},
-		margin: {
-			top: 8,
-			bottom: 20,
-			left: 20
-		}
-	},
-	xaxis: {
-		min: 0,
-		max: 1000
-	},
-	yaxis: {
-		min: 0,
-		max: 1000
-	},
-	legend: {
-		show: true
-	}
-};
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                          window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
 
-$.plot($("#cpu-usage-graph"), data, options);
+function GenerateCPUUsageGraphOptions() {
+	return {
+		grid: {
+			borderWidth: 1,
+			minBorderMargin: 20,
+			labelMargin: 10,
+			backgroundColor: {
+				colors: ["#fff", "#e4f4f4"]
+			},
+			margin: {
+				top: 8,
+				bottom: 20,
+				left: 20
+			}
+		},
+		legend: {
+			show: true
+		},
+		series: {
+			lines: {
+				fill: true,
+				// fillColor: "rgba(255, 0, 0, 1)",
+				show: true
+			}
+		},
+		xaxis: {
+			min: 0,
+			max: 1000
+		},
+		yaxis: {
+			min: 0,
+			max: 1000
+		}
+	};
+}
+
+function GetCPUUsageData() {
+	var data = [];
+	var allData = [];
+	for(var i = 0; i < 1000; i++) {
+		allData.push([i, Math.floor(Math.random() * 1000)]);
+	}
+	return [allData];
+}
+
+function PlotCPUUsageGraph() {
+	$.plot($("#cpu-usage-graph"), GetCPUUsageData(), cpuGraphOptions);
+	requestAnimationFrame(PlotCPUUsageGraph);
+}
+
+function TriggerCPUUsageTracking() {
+	requestAnimationFrame(PlotCPUUsageGraph);
+}
+
+var cpuGraphOptions = GenerateCPUUsageGraphOptions();
+TriggerCPUUsageTracking();
